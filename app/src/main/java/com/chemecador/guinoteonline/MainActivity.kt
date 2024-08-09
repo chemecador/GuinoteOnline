a78965fc3032b3d39df40ee44bd9e50c91c05572
chemecador <chemecador@gmail.com>
2024-08-10 01:02:14 +0200

    Partner and opponents cards

--- app/src/main/java/com/chemecador/guinoteonline/MainActivity.kt
+++ app/src/main/java/com/chemecador/guinoteonline/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,7 +40,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// List of all possible cards
 val cards = listOf(
     R.drawable.bastos1,
     R.drawable.bastos2,
@@ -78,7 +84,7 @@ val cards = listOf(
 )
 
 @Composable
-fun ShowRandomCards() {
+fun ShowPlayerCards(modifier: Modifier = Modifier) {
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp
     val cardWidth = (screenWidth * 2 / 13).dp
@@ -86,7 +92,7 @@ fun ShowRandomCards() {
     val selectedCards = remember { cards.shuffled().take(6) }
 
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(all = 4.dp),
         horizontalArrangement = Arrangement.SpaceBetween
@@ -104,14 +110,140 @@ fun ShowRandomCards() {
 }
 
 @Composable
-fun MainScreen() {
+fun ShowRightOpponentCards(modifier: Modifier = Modifier) {
+    val cardHeight = 60.dp
+    val rotations = listOf(120f, 105f, 90f, 75f, 60f, 45f)
+    val offsets = listOf(
+        (-40).dp,
+        (-32).dp,
+        (-24).dp,
+        (-16).dp,
+        (-8).dp,
+        0.dp
+    )
+
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter
+        modifier = modifier
+            .fillMaxHeight()
+            .wrapContentSize(align = Alignment.CenterEnd)
     ) {
-        ShowRandomCards()
+        repeat(6) { index ->
+            Image(
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "Right Opponent's card",
+                modifier = Modifier
+                    .offset(y = offsets[index], x = (-8).dp)
+                    .rotate(rotations[index])
+                    .height(cardHeight)
+                    .aspectRatio(0.75f)
+            )
+        }
     }
 }
+
+@Composable
+fun ShowLeftOpponentCards(modifier: Modifier = Modifier) {
+    val cardHeight = 60.dp
+    val rotations = listOf(-120f, -105f, -90f, -75f, -60f, -45f)
+    val offsets = listOf(
+        (-40).dp,
+        (-32).dp,
+        (-24).dp,
+        (-16).dp,
+        (-8).dp,
+        0.dp
+    )
+
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .wrapContentSize(align = Alignment.CenterStart)
+    ) {
+        repeat(6) { index ->
+            Image(
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "Left Opponent's card",
+                modifier = Modifier
+                    .offset(y = offsets[index], x = 8.dp)
+                    .rotate(rotations[index])
+                    .height(cardHeight)
+                    .aspectRatio(0.75f)
+            )
+        }
+    }
+}
+
+@Composable
+fun ShowPartnerCards(modifier: Modifier = Modifier) {
+    val cardWidth = 60.dp
+
+    val rotations = listOf(45f, 20f, 5f, -5f, -20f, -45f)
+    val offsets = listOf(
+        (-24).dp,
+        (-16).dp,
+        (-8).dp,
+        8.dp,
+        16.dp,
+        24.dp
+    )
+
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentSize(align = Alignment.Center)
+    ) {
+        repeat(6) { index ->
+            Image(
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "Opponent's card",
+                modifier = Modifier
+                    .height(cardWidth)
+                    .aspectRatio(0.75f)
+                    .rotate(rotations[index])
+                    .offset(x = offsets[index])
+
+            )
+        }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF0B6623))
+            .padding(top = 16.dp)
+    ) {
+
+        ShowPartnerCards(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 16.dp)
+        )
+
+        ShowPlayerCards(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        )
+
+        ShowRightOpponentCards(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 16.dp)
+        )
+
+        ShowLeftOpponentCards(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 16.dp)
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
